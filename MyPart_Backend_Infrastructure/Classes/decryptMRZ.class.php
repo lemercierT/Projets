a8fea259d6e2db789fee6387999c2836a7c2bd54
lemercierT <thibaultlemercier8@gmail.com>
2023-12-12 19:01:14 +0100

    ajout new id and new exec file for MRZ.php

--- MyPart_Backend_Infrastructure/Classes/decryptMRZ.class.php
+++ MyPart_Backend_Infrastructure/Classes/decryptMRZ.class.php
@@ -15,21 +15,11 @@
             $this->_imageMRZ = $imageMRZ;
         }
 
-        private function ImageToText(){
-            $this->_tesseract = new TesseractOCR($this->_imageMRZ);
-            $this->_text = $this->_tesseract->run();
-            $this->_text = str_replace([" ", "\n", "%"], "", $this->_text);
-        }
-
-        public function decryptInfos(){
-            $this->ImageToText();
-            $lenght = strlen($this->_text);
-            $id = "";
-            for($i = 0; $i < 2; $i++){
-                if($i < 2) $id .= $this->_text[$i];
-                if($id === "ID" && strlen($this->_text) == 72){
-                    return $this->IDdecrypt($this->_text);
-                }
+        private function stringReplace($string){
+            if($string == null) return $string;
+            else{
+                $n_string = trim(preg_replace('/[<>]/', "", $string));
+                return $n_string;
             }
         }
 
@@ -42,35 +32,124 @@
             return $ajust_annee."-".$mois."-".$jour;
         }
 
-        private function IDdecrypt($id_card){
-            $id_size = strlen($this->_text);
-            $id = "";
-            $issuing_state = "";
-            $lastname = "";
-            $document_nb = "";
-            $delivery_date = "";
-            $name = "";
-            $birthdate = "";
-            $gender = "";
+        private function ImageToText(){
+            $this->_tesseract = new TesseractOCR($this->_imageMRZ);
+            $this->_text = $this->_tesseract->run();
+            $this->_text = str_replace([" ", "\n", "%"], "", $this->_text);
+        }
 
-            for($i = 0; $i <= $id_size; $i++){
-                if($i < 2) $id .= $this->_text[$i];
-                if($i > 1 && $i < 5) $issuing_state .= $this->_text[$i];
-                if($i > 4 && $i < 30) $lastname .= $this->_text[$i];
-                if($i > 35 && $i < 40) $delivery_date .= $this->_text[$i];
-                if($i > 40 && $i < 59){
-                    if(!is_numeric($this->_text[$i])) $name .= $this->_text[$i];
-                }
-                if($i > 62 && $i < 69) $birthdate .= $this->_text[$i];
-                if($i > 69 && $i < 71) $gender .= $this->_text[$i];   
+        public function decryptInfos(){
+            $this->ImageToText();
+            $lenght = strlen($this->_text);
+            $documentCode = substr($this->_text, 0, 1);
+            switch($documentCode){
+                case "I":
+                    if($lenght == 72 && (substr($this->_text, 2, 3)) == "FRA") return $this->IDFrenchDecrypt($this->_text);
+                    else if($lenght == 90) return $this->IDdecryptTD1($this->_text);
+                    else return $this->IDdecryptTD2($this->_text);
+                case "P":
+                    return $this->PassportDecrypt($this->_text);
+                case "D":
+                    return $this->IDDriverLicense($this->_text);
             }
+            
+        }
 
-            echo $id." ".$issuing_state." ".$lastname." ".$delivery_date." ".$name." ".$birthdate." ".$gender;
+        private function IDFrenchDecrypt($mrz){
+            $documentCode1 = substr($mrz, 0, 1);
+            $documentCode2 = substr($mrz, 1, 1);
+            $issuing_state = substr($mrz, 2, 3);
+            $lastname = preg_replace('/[<>]/', "", substr($mrz, 5, 25));
+            $first_second_name = explode("<<", substr($mrz, 49, 14));
+            $firstname = trim($first_second_name[0]);
+            $second_name = trim($first_second_name[1]);
+            $birthdate = $this->dateToSQLDate(substr($mrz, 63, 6));
 
-            $lastname = preg_replace(['/[<>]/'], "", $lastname);
-            $name = preg_replace(['/[<>]/'], "", $name);
-            $birthdate = $this->dateToSQLDate($birthdate);
-            return new Client($lastname, $name, $birthdate);
+            $id["documentCode"] = ($documentCode1 == "I") ? "ID-1" : "Unknown";
+            $id["documentCode"] = ($documentCode2 == "R") ? "Residence Card" : "";
+            $id["issuing_state"] = $issuing_state;
+            $id["lastname"] = $lastname;
+            $id["firstname"] = $firstname;
+            $id["second_name"] = $second_name;
+            $id["birthdate"] = $birthdate;
+            
+
+            echo $documentCode1." ".$documentCode2." ".$issuing_state." ".$lastname." ".$firstname." ".$second_name." ".$birthdate;
+
+            return new Client($id["lastname"], $id["firstname"], $id["birthdate"]);
+        }
+
+        private function IDdecryptTD1($mrz){
+            $documentCode1 = substr($mrz, 0, 1);
+            $documentCode2 = substr($mrz, 1, 1);
+            $issuing_state = $this->stringReplace(substr($mrz, 2, 3));
+            $names = explode("<<", substr($mrz, 60, 30));
+            $lastname = $names[0];
+            $firstname = $names[1];
+            $nationality = $this->stringReplace(substr($mrz, 45, 3));
+            $birthdate = $this->dateToSQLDate(substr($mrz, 30, 6));
+
+            $id["documentCode"] = ($documentCode1 == "I") ? "ID-1" : "Unknown";
+            $id["documentCode"] = ($documentCode2 == "R") ? "Residence Card" : "";
+            $id["issuing_state"] = $issuing_state;
+            $id["lastname"] = $lastname;
+            $id["firstname"] = $firstname;
+            $id["nationality"] = $nationality;
+            $id["birthdate"] = $birthdate;
+            
+
+            echo $documentCode1." ".$documentCode2." ".$issuing_state." ".$lastname." ".$firstname." ".$birthdate;
+
+            return new Client($id["lastname"], $id["firstname"], $id["birthdate"]);
+        }
+
+        private function IDdecryptTD2($mrz){
+            $documentCode1 = substr($mrz, 0, 1);
+            $documentCode2 = substr($mrz, 1, 1);
+            $issuing_state = $this->stringReplace(substr($mrz, 2, 3));
+            $names = explode("<<", substr($mrz, 5, 31));
+            $lastname = $names[0];
+            $firstname = $names[1];
+            $nationality = $this->stringReplace(substr($mrz, 46, 3));
+            $birthdate = $this->dateToSQLDate(substr($mrz, 49, 6));
+
+            $id["documentCode"] = ($documentCode1 == "I") ? "ID-2" : "Unknown";
+            $id["documentCode"] = ($documentCode2 == "R") ? "Residence Card" : "";
+            $id["issuing_state"] = $issuing_state;
+            $id["lastname"] = $lastname;
+            $id["firstname"] = $firstname;
+            $id["nationality"] = $nationality;
+            $id["birthdate"] = $birthdate;
+            
+
+            echo $documentCode1." ".$documentCode2." ".$issuing_state." ".$lastname." ".$firstname." ".$birthdate;
+
+            return new Client($id["lastname"], $id["firstname"], $id["birthdate"]);
+        }
+
+        private function PassportDecrypt($mrz){
+            $documentCode = substr($mrz, 0, 1);
+            $issuing_state = substr($mrz, 2, 3);
+            $names = explode("<<", substr($mrz, 5, 39));
+            $lastname = $names[0];
+            $firstname = $names[1];
+            $nationality = $this->stringReplace(substr($mrz, 54, 3));
+            $birthdate = $this->dateToSQLDate(substr($mrz, 57, 6));
+
+            $id["documentCode"] = $documentCode;
+            $id["issuing_state"] = $issuing_state;
+            $id["lastname"] = $lastname;
+            $id["firstname"] = $firstname;
+            $id["nationality"] = $nationality;
+            $id["birthdate"] = $birthdate;
+
+            echo $id["documentCode"]." ".$id["issuing_state"]." ".$id["lastname"]." ".$id["birthdate"]." ".$id["nationality"];
+
+            return new Client($id["lastname"], $id["firstname"], $id["birthdate"]);
+        }
+
+        private function IDDriverLicense($mrz){
+
         }
     }
 ?>
