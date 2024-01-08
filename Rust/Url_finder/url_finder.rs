aeab1ab144a3894e116262f6a33aa7f06088ec8f
LEMERCIER Thibault <thibault.lemercier7@etu.univ-lorraine.fr>
2024-01-08 17:11:45 +0100

    entre temps j'ai appris le rust ;)

--- Rust/Url_finder/url_finder.rs
+++ Rust/Url_finder/url_finder.rs
@@ -1,81 +1,98 @@
 use std;
-use std::error::Error;
 use regex::Regex;
 
-fn input_url() -> Result<String, Box<dyn Error>> {
-    let mut url = String::new();
+fn input_url() -> String {
+    let mut url: String = String::with_capacity(20);
     println!("[x] Enter URL : ");
 
-    match std::io::stdin().read_line(&mut url){
-        Ok(_) => {
-            let res: String = url.trim().to_string();
-            if res != ""{
-                Ok(res)
-            }else {
-                Err("".into())
+    while url.is_empty(){
+        let mut buffer = String::new();
+        if let Ok(_) = std::io::stdin().read_line(&mut buffer){
+            if buffer.trim().is_empty(){
+                println!("[-] Empty URL");
+            }else{
+                url = buffer.trim().to_string();
             }
-        }
-        Err(error) => {
-            Err(Box::new(error))
+        }else{
+            println!("[-] Error std::io::stdin");
         }
     }
+
+    return url;
 }
 
-fn choose_attack_mod() -> Result<String, Box<dyn Error>>{
-    let mut choice: String = String::new();
+fn choose_attack_mod() -> String{
+    let mut choice: String = String::with_capacity(1);
     println!("[x] Choose you attack mod : \n[1] Wordlist\n[2] Bruteforce");
 
-    match std::io::stdin().read_line(&mut choice){
-        Ok(_) => {
-            let res: String = choice.trim().to_string();
-            if res != ""{
-                Ok(res)
+    while choice.is_empty(){
+        let mut buffer: String = String::with_capacity(1);
+        if let Ok(_) = std::io::stdin().read_line(&mut buffer){
+            if buffer.trim().is_empty(){
+                println!("[-] Empty number");
             }else{
-                Err("".into())
+                let check_number: Regex = Regex::new(r"[12]").unwrap();
+                if check_number.is_match(buffer.trim()){
+                    choice = buffer.trim().to_string();
+                }else{
+                    println!("[-] Not valid number");
+                }
             }
-        }
-        Err(error) => {
-            Err(Box::new(error))
+        }else{
+            println!("[-] Error std::io::stdin");
         }
     }
+
+    return choice;
 }
 
-fn wordlist_attack() ->  Result<String, Box<dyn Error>>{
+fn wordlist_attack() ->  String{
     let mut path_wordlist: String = String::new();
     println!("[x] Enter the path of your wordlist : \nExample : /usr/share/wordlist/rockyou.txt");
 
-    match std::io::stdin().read_line(&mut path_wordlist){
-        Ok(_) => {
-            Ok(path_wordlist.trim().to_string())
-        }
-        Err(error) => {
-            Err(Box::new(error))
+    while path_wordlist.trim().is_empty(){
+        let mut buffer: String = String::new();
+        if let Ok(_) = std::io::stdin().read_line(&mut buffer){
+            if buffer.trim().is_empty(){
+                println!("[-] Empty input !")
+            }else{
+                path_wordlist = buffer;
+            }
+        }else{
+            println!("[-] Error std::io::stdin");
         }
     }
+
+    return path_wordlist;
 }
 
 // fn url_wordlist_attack(url: String, path_wordlist: String) -> Result<String, Box<dyn Error>>{
 //     // Attentre wordlist_attack
 // }
 
-fn bruteforce_attack() -> Result<String, Box<dyn Error>>{
+fn bruteforce_attack() -> String{
     let mut selected_char: String = String::new();
     println!("[x] Enter character you want : \nExample : [a-f][0-9]");
 
-    match std::io::stdin().read_line(&mut selected_char){
-        Ok(_) => {
-            let char_regex: Regex = Regex::new(r"[a-zA-Z0-9\[\]]").unwrap();
-            if char_regex.is_match(&selected_char){
-                Ok(selected_char.trim().to_string())
+    while selected_char.is_empty(){
+        let mut buffer: String = String::new();
+        if let Ok(_) = std::io::stdin().read_line(&mut buffer){
+            if buffer.trim().is_empty(){
+                println!("[-] Empty input !");
+            }else{
+                let char_regex: Regex = Regex::new(r"[a-zA-Z0-9\[\]]").unwrap();
+                if char_regex.is_match(buffer.trim()){
+                    selected_char = buffer;
+                }else{
+                    println!("[-] Invalid character !");
+                }
             }
-            else{
-                Err("".into())
-            }
-        }
-        Err(error) => {
-            Err(Box::new(error))
+        }else{
+            println!("[-] Error std::io::stdin");
         }
     }
+
+    return selected_char;
 }
 
 // fn url_bruteforce_attack(url: String, selected_char: String) -> Result<String, Box<dyn Error>>{
@@ -83,53 +100,17 @@ fn bruteforce_attack() -> Result<String, Box<dyn Error>>{
 // }
 fn main(){
     let mut _url: String = String::new();
-    match input_url(){
-        Ok(url) => {
-            _url = url;
-            println!("[+] URL selected : {}", _url);
-        }
-        Err(error) => {
-            let _error: Box<dyn Error> = error;
-            println!("[-] Error selected URL : {}", _error);
-        }
-    }
+    _url = input_url();
 
     let mut _choice: String = String::new();
-    match choose_attack_mod(){
-        Ok(choice) => {
-            _choice = choice;
-            println!("[+] Choice selected : {}", _choice);
-        }
-        Err(error) => {
-            let mut _error: Box<dyn Error> = error;
-            println!("[-] Error choice => use 1 or 2 : {}", _error);
-        }
-    }
+    _choice = choose_attack_mod();
 
     let mut _path_wordlist: String = String::new();
     let mut _selected_char: String = String::new();
     if _choice == "1"{
-        match wordlist_attack(){
-            Ok(path_wordlist) => {
-                println!("[+] Wordlist selected : {}", path_wordlist);
-                _path_wordlist = path_wordlist;
-            }
-            Err(error) => {
-                let _error: Box<dyn Error> = error;
-                println!("[-] Error searching wordlist : {}", _error);
-            }
-        }
+        _path_wordlist = wordlist_attack();
     }else if _choice == "2" {
-        match bruteforce_attack(){
-            Ok(selected_char) => {
-                println!("[+] Selected character : {}", selected_char);
-                _selected_char = selected_char;
-            }
-            Err(error) => {
-                let _error: Box<dyn Error> = error;
-                println!("[-] Error selected character : {}", _error);
-            }
-        }
+        _selected_char = bruteforce_attack();
     }
 
 }
